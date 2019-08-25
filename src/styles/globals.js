07eb2a95139c39495cca4aae95d42cddc7516887
Olivier Nsabimana <onsabimana@cozero.com.au>
2019-08-26 09:30:06 +1000

    Init

---
+++ src/styles/globals.js
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from 'styled-components';
+import { colorsDark } from './palette';
+
+
+const setGlobalStyles = () =>
+  createGlobalStyle`
+    * {
+      box-sizing: border-box;
+    }
+    html, body {
+      font-family: lato,Helvetica-Neue,Helvetica,Arial,sans-serif;
+      width: 100vw;
+      overflow-x: hidden,
+      margin: 0;
+      padding: 0;
+      min-height: 100vh;
+      background-color: ${colorsDark.background};
+    }
+    ul {
+      list-style: none;
+      padding: 0;
+    }
+    a {
+      text-decoration: none;
+      &:visited {
+        color: inherit;
+      }
+    }
+  `;
+
+export default setGlobalStyles;
