806dfd27015c2d43937c791a4c06641ea2ed7f67
Olivier Nsabimana <onsabimana@cozero.com.au>
2019-08-28 16:26:49 +1000

    Updates

---
+++ precache-manifest.d594fb73363c35f222ee5d1a78165370.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51079d166cca1c2b3c45e1947d88e45e",
+    "url": "/hacker-news-reader/index.html"
+  },
+  {
+    "revision": "7e98e0d24cf4e5811bfc",
+    "url": "/hacker-news-reader/static/js/2.acd71526.chunk.js"
+  },
+  {
+    "revision": "d0cd934e1ced3e7f5f4a",
+    "url": "/hacker-news-reader/static/js/main.5e131ace.chunk.js"
+  },
+  {
+    "revision": "edac856f03860d54c4f1",
+    "url": "/hacker-news-reader/static/js/runtime~main.0a346fbc.js"
+  }
+]);
